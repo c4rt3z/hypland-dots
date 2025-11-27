339c0d6ac67344d7149b660797e3693248148906
c4rt3z <amogus880333@gmail.com>
2025-11-27 22:31:49 +0300

    Add files via upload

---
+++ scripts/installer/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get the directory of the current script
+BASE_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../")
+
+# Source helper file
+source $BASE_DIR/scripts/installer/helper.sh
+
+# Trap for unexpected exits
+trap 'trap_message' INT TERM
+
+# Script start
+log_message "Installation started."
+print_bold_blue "\nHyrland dots by Cartez"
+echo "---------------"
+
+# Check if running as root
+check_root
+
+check_os
+
+# Run child scripts
+run_script "prerequisites.sh" "Prerequisites Setup"
+run_script "hypr.sh" "Hyprland & Critical Softwares Setup"
+run_script "utilities.sh" "Basic Utilities & Configs Setup"
+run_script "theming.sh" "Themes and Tools Setup"
+run_script "final.sh" "Final Setup"
+
+print_bold_blue "\nSetup Complete\n"
+log_message "Installation completed successfully."
